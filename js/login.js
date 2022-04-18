93b440c1027a8d2ae6adbad7f6fb59d020e08779
Parassahu2308 <sahujuparas786@gmail.com>
2022-04-18 07:53:11 +0530

    updated

---
+++ js/login.js
@@ -0,0 +1,18 @@
+
+// ------> Login  <-------
+let login = document.getElementById("signbtn");
+login.addEventListener("click", function(e){
+  let user = document.getElementById("usertxt").value;
+  let pass = document.getElementById("passtxt").value;
+
+  if(user == 'parassahu' && pass =='12345678' ){
+    window.location.href="/dash.html";
+  }
+  else{
+    const error = document.createElement("p");
+    error.innerHTML = "Invalid Password! Try Again.";
+    document.getElementById("error").appendChild(error);
+  }
+  e.preventDefault();
+});
+
